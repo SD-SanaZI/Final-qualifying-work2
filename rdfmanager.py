bfef3271f39f4f4fed10f10d45821915632f51ca
Drozd_AS <31595916+SD-SanaZI@users.noreply.github.com>
2023-04-11 15:10:45 +0300

    Update rdfmanager.py
    Добавлен сбор статистики нагрузки CPU
    Стандартизирован запуск потоков

--- rdfmanager.py
+++ rdfmanager.py
@@ -1,44 +1,125 @@
 from pymantic import sparql
 import time
+import psutil
 import threading
+import statistics
+import matplotlib.pyplot as plt
 
 server = sparql.SPARQLServer('http://127.0.0.1:9999/blazegraph/sparql')
 
-'''
-server.update('load <file:///E:/KG_telecom_actions_1_.nq>')
-server.update('load <file:///E:/KG_telecom_device_1_.nq>')
-'''
+arrayDelete = [['delete where {<http://127.0.0.1/Core_1_Level_2_1/> <http://127.0.0.1/bg/ont/test1#has_id> ?o}',
+         'delete where {<http://127.0.0.1/Core_1_Level_2_1000/> <http://127.0.0.1/bg/ont/test1#has_id> ?o}',
+         'delete where {<http://127.0.0.1/Core_1_Level_2_1001/> <http://127.0.0.1/bg/ont/test1#has_id> ?o}',
+         'delete where {<http://127.0.0.1/Core_1_Level_2_1002/> <http://127.0.0.1/bg/ont/test1#has_parent_id> ?o}',
+         'delete where {<http://127.0.0.1/Core_1_Level_2_1003/> <http://127.0.0.1/bg/ont/test1#has_id> ?o}',
+         'delete where {<http://127.0.0.1/Core_1_Level_2_1004/> <http://127.0.0.1/bg/ont/test1#has_id> ?o}',
+         'delete where {<http://127.0.0.1/Core_1_Level_2_1005/> <http://127.0.0.1/bg/ont/test1#linked_to> ?o}',
+         'delete where {<http://127.0.0.1/Core_1_Level_2_1006/> <http://127.0.0.1/bg/ont/test1#has_id> ?o}',
+         'delete where {<http://127.0.0.1/Core_1_Level_2_50114/> <http://127.0.0.1/bg/ont/test1#has_parent_id> ?o}',
+         'delete where {<http://127.0.0.1/Core_1_Level_2_12128/> <http://127.0.0.1/bg/ont/test1#linked_to> ?o}'
+         ]]
 
 
-def query(queryTxt):
-    start = time.time()
-    res1 = server.query(queryTxt)
-    end = time.time()
-    return [res1, end - start]   
-   
 
-def request():
-    server1 = sparql.SPARQLServer('http://127.0.0.3:9999/blazegraph/sparql')
-    print('Request started')
-    start = time.time()
-    server1.query('select * where {?s ?p ?o} limit 200000')
-    end = time.time()
-    print(end - start)
+arrayInsert = [["insert data {<http://127.0.0.1/Core_1_Level_2_1/> <http://127.0.0.1/bg/ont/test1#has_id> 'Core_1_Level_2_1'}",
+          "insert data {<http://127.0.0.1/Core_1_Level_2_1000/> <http://127.0.0.1/bg/ont/test1#has_id> 'Core_1_Level_2_1000'}",
+          "insert data {<http://127.0.0.1/Core_1_Level_2_1001/> <http://127.0.0.1/bg/ont/test1#has_id> 'Core_1_Level_2_1001'}",
+          "insert data {<http://127.0.0.1/Core_1_Level_2_1002/> <http://127.0.0.1/bg/ont/test1#has_parent_id> <http://127.0.0.1/Core_1/>}",
+          "insert data {<http://127.0.0.1/Core_1_Level_2_1003/> <http://127.0.0.1/bg/ont/test1#has_id> 'Core_1_Level_2_1003'}",
+          "insert data {<http://127.0.0.1/Core_1_Level_2_1004/> <http://127.0.0.1/bg/ont/test1#has_id> 'Core_1_Level_2_1004'}",
+          "insert data {<http://127.0.0.1/Core_1_Level_2_1005/> <http://127.0.0.1/bg/ont/test1#linked_to> <http://127.0.0.1/Core_2_Level_2_1005/>}",
+          "insert data {<http://127.0.0.1/Core_1_Level_2_1006/> <http://127.0.0.1/bg/ont/test1#has_id> 'Core_1_Level_2_1006'}",
+          "insert data {<http://127.0.0.1/Core_1_Level_2_50114/> <http://127.0.0.1/bg/ont/test1#has_parent_id> <http://127.0.0.1/Core_1/>}",
+          "insert data {<http://127.0.0.1/Core_1_Level_2_12128/> <http://127.0.0.1/bg/ont/test1#linked_to> <http://127.0.0.1/Core_2_Level_2_12128/>}"
+          ]]
 
-timer = time.time()
-timeCounter = timer
-array = []
-currentThreadNum = 0  
-threadcount = 20    #количество потоков
-timeOfWorking = 5   #время работы
-delta = 0.1          #переодичность запуска потоков
+arraySelect = [['select * where {?s ?p ?o} limit 50000','select * where {?s ?p ?o} limit 50000']]
 
-for i in range(threadcount):
-    array.append(threading.Thread(target=request))
-while(time.time() < timer + timeOfWorking):
-    if(time.time() > timeCounter + delta):
-        timeCounter = time.time()
-        if(currentThreadNum < threadcount - 1):
-            currentThreadNum = currentThreadNum + 1
-            print(currentThreadNum, time.time() - timer) #выводит номер потока и время запуска
-            array[currentThreadNum].start()
+flagDeleteThread = 0
+numderDeleteArray = 0
+flagSelectThread = 1
+numderSelectArray = 0
+flagCPUThread = 1
+arrayThreads = []
+
+def deleteRequest():
+    number = numderDeleteArray
+    executionTime = []
+    for i in range(len(arrayDelete[number])):
+        print("Delete request №" + str(i))
+        start = time.time()
+        #Удаление записи
+        server.update(arrayDelete[number][i])
+        end = time.time()
+        executionTime.append(end - start)
+    print("execution time of deletion requests: " + str(executionTime))
+    print("average execution time: " + str(statistics.mean(executionTime)))
+
+def selextRequest():
+    number = numderDeleteArray
+    executionTime = []
+    for i in range(len(arraySelect[number])):
+        print("Select request №" + str(i))
+        start = time.time()
+        #Удаление записи
+        server.query(arraySelect[number][i])
+        end = time.time()
+        executionTime.append(end - start)
+    print("execution time of deletion requests: " + str(executionTime))
+    print("average execution time: " + str(statistics.mean(executionTime)))
+
+def controlCPU():
+    cpuArray = []
+    timer = time.time()
+    delta = 1
+    while(flagCPUThread == 1):   
+        if(time.time() > timer + delta):
+            cpuArray.append(psutil.cpu_percent(interval=1, percpu=False))
+            timer = time.time()
+    print("cpu load: " + str(cpuArray))
+    if(len(cpuArray) > 0):
+        print("average cpu load: " + str(statistics.mean(cpuArray)))    #Выводит среднее время ответа
+    
+    
+
+if(flagCPUThread == 1):
+    thread = threading.Thread(target=controlCPU)
+    thread.start()
+
+#Запустит первый список selrct запросов из arraySelect на одном потоке
+if(flagSelectThread == 1):
+    numderSelectArray = 0
+    numberThread = len(arrayThreads)
+    arrayThreads.append(threading.Thread(target=selextRequest))
+    arrayThreads[numberThread].start()
+
+#Запустит все списки selrct запросов из arraySelect на разных потоках
+if(flagSelectThread == 2):
+    numberThread = len(arrayThreads)
+    for i in range(len(arrayDelete)):
+        arrayThreads.append(threading.Thread(target=selextRequest))
+    for i in range(len(arrayDelete)):
+        numderSelectArray = i
+        arrayThreads[numberThread + i].start()
+
+#Запустит первый список запросов удаления из arrayDelete на одном потоке
+if(flagDeleteThread == 1):
+    numderDeleteArray = 0
+    numberThread = len(arrayThreads)
+    arrayThreads.append(threading.Thread(target=deleteRequest))
+    arrayThreads[numberThread].start()
+
+#Запустит все списки запросов удаления из arrayDelete на разных потоках
+if(flagDeleteThread == 2):
+    numberThread = len(arrayThreads)
+    for i in range(len(arrayDelete)):
+        arrayThreads.append(threading.Thread(target=deleteRequest))
+    for i in range(len(arrayDelete)):
+        numderDeleteArray = i
+        arrayThreads[numberThread + i].start()
+
+ 
+if(flagCPUThread == 1):
+    for thread in arrayThreads:
+        thread.join()
+    flagCPUThread = 0
